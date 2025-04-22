867ceeccaeb00a4256355a98d9af61cf6a40de8d
Magda <hromagda@centrum.cz>
2025-04-22 18:05:37 +0200

    Aplikace jako PWA

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,16 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(
+            (registration) => {
+                console.log('Service Worker registrado con éxito:', registration);
+            },
+            (error) => {
+                console.log('Error al registrar el Service Worker:', error);
+            }
+        );
+    });
+}
